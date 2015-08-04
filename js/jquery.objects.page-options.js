13cebf55a79a50e80739b4501af89249e2131f25
devcorrelator <frenchbm@hmturnercoopw7e.od.nih.gov>
2015-08-04 14:43:29 -0400

    Added the init function I use called refresh.

--- js/jquery.objects.page-options.js
+++ js/jquery.objects.page-options.js
@@ -14,6 +14,28 @@ $.widget( "objects.objectPageOptions", $.objects.objectsBase,
 		this.element
 			.removeClass( "objects-page-options" );
 	},
+	refresh: function() {
+		
+		// go through each item (ul>li)
+		var self = this;
+		self.element.find('ul.page-options-main li.page-options-main').each(function(){
+			
+			// attach a tooltipster to each of the titles that have a list present
+			var tooltip_content = $(this).find('.list-wrapper');
+			if(tooltip_content.length)
+			{
+				$(this).click(function(event){ event.preventDefault(); });
+				if ( $.isFunction($.fn.tooltipster) ) 
+				{
+					$(this).tooltipster({
+						content: tooltip_content,
+						theme: 'tooltipster-shadow tooltipster-page-options',
+					});
+				}
+				tooltip_content.hide();
+			}
+		});
+	},
 
 });
 
